a563f2d8cf1d2682c50a3a8c1f64a9a6dbdebceb
Pascal Pieper <accounts@pascalpieper.de>
2022-11-10 17:53:56 +0100

    Improved set-time usability (mainly response time). Untested.

--- src/set_time.cpp
+++ src/set_time.cpp
@@ -2,19 +2,20 @@
 #include "display.hpp"
 #include "rtcstuff.h"
 
-enum class State : uint8_t
-{
-    day = 0,
-    month,
-    year,
-    hour,
-    minute,
-    save    //important to be at the end
-};
+static const char save_msg[] = "Press 'set' again to save";
 
 void set_time()
 {
-    State state = static_cast<State>(0);
+    enum class State : uint8_t
+    {
+        day = 0,
+        month,
+        year,
+        hour,
+        minute,
+        save    //important to be at the end
+    } state = State::day;
+    
     char buf[9];
     uint8_t frame = 0;
     RtcDateTime new_time = now;
@@ -22,22 +23,14 @@ void set_time()
     bool up_button_pressed = false;
     uint8_t cycles_up_button_pressed = 0;
 
-
     while(true)
     {
         //action
-        switch(state)
+        if(up_button_pressed)
         {
-            case State::hour:
-                snprintf_P(buf,
-                        sizeof(buf),
-                        PSTR("%02u:%02u:%02u"),
-                        new_time.Hour(),
-                        new_time.Minute(),
-                        new_time.Second()
-                );
-                if(up_button_pressed)
-                {
+            switch(state)
+            {
+                case State::hour:
                     new_time = RtcDateTime(
                         new_time.Year(),
                         new_time.Month(),
@@ -46,23 +39,8 @@ void set_time()
                         new_time.Minute(),
                         new_time.Second()
                     );
-                }
-                if(frame % 2)
-                {
-                    buf[0] = ' ';
-                    buf[1] = ' ';
-                }
-                break;
-            case State::minute:
-                snprintf_P(buf,
-                        sizeof(buf),
-                        PSTR("%02u:%02u:%02u"),
-                        new_time.Hour(),
-                        new_time.Minute(),
-                        new_time.Second()
-                );
-                if(up_button_pressed)
-                {
+                    break;
+                case State::minute:
                     new_time = RtcDateTime(
                         new_time.Year(),
                         new_time.Month(),
@@ -71,23 +49,8 @@ void set_time()
                         (new_time.Minute() + 1) % 60,
                         new_time.Second()
                     );
-                }
-                if(frame % 2)
-                {
-                    buf[3] = ' ';
-                    buf[4] = ' ';
-                }
-                break;
-            case State::day:
-                snprintf_P(buf,
-                        sizeof(buf),
-                        PSTR("%02u.%02u.%02u"),
-                        new_time.Day(),
-                        new_time.Month(),
-                        new_time.Year()%100
-                );
-                if(up_button_pressed)
-                {
+                    break;
+                case State::day:
                     new_time = RtcDateTime(
                         new_time.Year(),
                         new_time.Month(),
@@ -96,23 +59,8 @@ void set_time()
                         new_time.Minute(),
                         new_time.Second()
                     );
-                }
-                if(frame % 2)
-                {
-                    buf[0] = ' ';
-                    buf[1] = ' ';
-                }
-                break;
-            case State::month:
-                snprintf_P(buf,
-                        sizeof(buf),
-                        PSTR("%02u.%02u.%02u"),
-                        new_time.Day(),
-                        new_time.Month(),
-                        new_time.Year()%100
-                );
-                if(up_button_pressed)
-                {
+                    break;
+                case State::month:
                     new_time = RtcDateTime(
                         new_time.Year(),
                         (new_time.Month() + 1) % 12,
@@ -121,23 +69,8 @@ void set_time()
                         new_time.Minute(),
                         new_time.Second()
                     );
-                }
-                if(frame % 2)
-                {
-                    buf[3] = ' ';
-                    buf[4] = ' ';
-                }
-                break;
-            case State::year:
-                snprintf_P(buf,
-                        sizeof(buf),
-                        PSTR("%02u.%02u.%02u"),
-                        new_time.Day(),
-                        new_time.Month(),
-                        new_time.Year()%100
-                );
-                if(up_button_pressed)
-                {
+                    break;
+                case State::year:
                     new_time = RtcDateTime(
                         2000 + ((new_time.Year() + 1) % 100),
                         new_time.Month(),
@@ -146,21 +79,66 @@ void set_time()
                         new_time.Minute(),
                         new_time.Second()
                     );
-                }
-                if(frame % 2)
-                {
-                    buf[6] = ' ';
-                    buf[7] = ' ';
-                }
-                break;
-            case State::save:
-                memcpy(buf, PSTR("  SAVE? "), 9);
-                break;
+                    break;
+                default:
+                    break;
+            }
         }
 
+        // display update
+        switch(state)
+        {
+        case State::hour:
+        case State::minute:
+            snprintf_P(buf,
+                sizeof(buf),
+                PSTR("%02u:%02u:%02u"),
+                new_time.Hour(),
+                new_time.Minute(),
+                new_time.Second()
+            );
+            break;
+        case State::day:
+        case State::month:
+        case State::year:
+            snprintf_P(buf,
+                sizeof(buf),
+                PSTR("%02u.%02u.%02u"),
+                new_time.Day(),
+                new_time.Month(),
+                new_time.Year()%100
+            );
+        case State::save:
+            memcpy(buf, PSTR("  SAVE? "), 9);
+            break;
+        }
+        
+        if(!up_button_pressed && (frame % 2))
+        {
+            // blank current value like a "cursor"
+            switch(state)
+            {
+            case State::hour:
+            case State::day:
+                buf[0] = ' ';
+                buf[1] = ' ';
+                break;
+            case State::minute:
+            case State::month:
+                buf[3] = ' ';
+                buf[4] = ' ';
+                break;
+            case State::year:
+                buf[6] = ' ';
+                buf[7] = ' ';
+                break;
+            default:
+                break;
+            }
+        }
         auto_show(buf, 8);
 
-        //transition
+        //transition "next button"
         if(getButtonDebounce(nextButton))
         {
             switch(state)
@@ -169,17 +147,21 @@ void set_time()
                     now = new_time;
                     Rtc.SetDateTime(new_time);
                     return; //END
+                case static_cast<State>(static_cast<uint8_t>(State::save) - 1):
+                    auto_show(save_msg,sizeof(save_msg));
                 default:
                     state = static_cast<State>(static_cast<uint8_t>(state) + 1);
             }
         }
 
+
+        // Set-Button "long press" Input handling
         frame++;
         bool was_button_pressed = up_button_pressed;
         up_button_pressed = false;
 
         for(uint8_t i = 0; i < (cycles_up_button_pressed > 3 ? 10 : 50) ; i++)
-        {   //poor mans buttoncheck
+        {   //poor man's 'still pressing' buttoncheck
             up_button_pressed |= getButton(upButton);
             delay(10);
         }
